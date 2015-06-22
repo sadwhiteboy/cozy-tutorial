8bac58ffb649c67289ba81dc3fd6c54e1b53312f
sadwhiteboy <thesadwhiteboy@gmail.com>
2015-06-21 23:44:30 -0400

    broken; needs fixing
    dickbutt and cozy get a flat tire on their bus ride to new york.

--- server.js
+++ server.js
@@ -6,6 +6,29 @@ var express = require('express')
 
 var app = express();
 
+/* We add directives to tell express to use Jade to
+   render templates */
+app.configure(function() {
+  app.set('views', __dirname + '/public');
+  app.engine('.html', require('jade').__express);
+});
+
+// Let's define some bookmarks
+var bookmarks = [];
+bookmarks.push({title: "Cozycloud", url: "https://cozycloud.cc"});
+bookmarks.push({title: "Cozy.io", url: "http://cozy.io"});
+bookmarks.push({title: "My Cozy", url: "http://localhost:9104/"});
+
+// We render the templates with the data
+app.get('/', function(req, res) {
+  var params = {
+    "bookmarks": bookmarks
+  }
+  res.render('index.jade', params, function(err, html) {
+    res.send(200, html);
+  });
+});
+
 /* This will allow Cozy to run your app smoothly but
  it won't break other execution environment */
 var port = process.env.PORT || 9250;
@@ -15,9 +38,4 @@ var host = process.env.HOST || "127.0.0.1";
 var server = http.createServer(app).listen(port, host, function() {
   console.log("Server listening to %s:%d within %s environment",
               host, port, app.get('env'));
-});
-
-// At the root of your website, we show the index.html page
-app.get('/', function(req, res) {
-  res.sendfile('./public/index.html')
 });
